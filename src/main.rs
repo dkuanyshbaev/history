9094e307f630051b41098170ee14d81f910b4d75
Denis K <konsistentsi@gmail.com>
2023-05-05 23:11:59 +0700

    ADD: auth

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,20 @@ use axum::{
     http::StatusCode,
     response::{Html, IntoResponse, Response},
     routing::get,
-    Router,
+    Extension, Router,
 };
-use std::{env, net::SocketAddr, sync::Arc};
-
-//-----------------------------------------------------
-use std::collections::HashMap;
-
-use axum_login::{
-    axum_sessions::async_session::MemoryStore as SessionMemoryStore, extractors::AuthContext,
-    memory_store::MemoryStore as AuthMemoryStore,
-};
-use tokio::sync::RwLock;
-//-----------------------------------------------------
-use axum::Extension;
 use axum_login::{
     axum_sessions::{async_session::MemoryStore, SessionLayer},
+    extractors::AuthContext,
+    memory_store::MemoryStore as AuthMemoryStore,
     secrecy::SecretVec,
-    AuthLayer, AuthUser, RequireAuthorizationLayer, SqliteStore,
+    AuthLayer, AuthUser, RequireAuthorizationLayer,
 };
 use rand::Rng;
-use sqlx::sqlite::SqlitePoolOptions;
+use std::{collections::HashMap, env, net::SocketAddr, sync::Arc};
+use tokio::sync::RwLock;
 
-#[derive(Debug, Clone, sqlx::FromRow)]
+#[derive(Debug, Clone)]
 struct User {
     id: usize,
     name: String,
@@ -33,9 +24,20 @@ struct User {
     role: Role,
 }
 
+impl User {
+    pub fn new_admin(secret: String) -> Self {
+        Self {
+            id: 42,
+            name: "Admin".to_string(),
+            password_hash: secret,
+            role: Role::Admin,
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 enum Role {
-    User,
+    _User,
     Admin,
 }
 
@@ -53,18 +55,8 @@ impl AuthUser<usize, Role> for User {
     }
 }
 
-// type AuthContext = axum_login::extractors::AuthContext<i64, User, SqliteStore<User>>;
-// type Auth = AuthContext<usize, User, AuthMemoryStore<usize, User>>;
-// type RequireAuth = RequireAuthorizationLayer<usize, User>;
-
 type Auth = AuthContext<usize, User, AuthMemoryStore<usize, User>, Role>;
 type RequireAuth = RequireAuthorizationLayer<usize, User, Role>;
-
-//-----------------------------------------------------------
-
-struct HistoryState {
-    secret: String,
-}
 
 struct Book {
     name: String,
@@ -80,52 +72,26 @@ struct Post {
 #[tokio::main]
 async fn main() {
     let secret = env::var("SECRET").expect("SECRET must be set!");
-    let state = Arc::new(HistoryState { secret });
-
-    //-------------------------------------------------
-    let secret = rand::thread_rng().gen::<[u8; 64]>();
+    let session_secret = rand::thread_rng().gen::<[u8; 64]>();
     let session_store = MemoryStore::new();
-    let session_layer = SessionLayer::new(session_store, &secret);
+    let session_layer = SessionLayer::new(session_store, &session_secret);
 
-    //------------------
     let store = Arc::new(RwLock::new(HashMap::default()));
-    let user = User {
-        id: 42,
-        name: "Denis".to_string(),
-        password_hash: "password".to_string(),
-        role: Role::Admin,
-    };
+    let user = User::new_admin(secret);
     store.write().await.insert(user.get_id(), user);
-
     let user_store = AuthMemoryStore::new(&store);
-    let auth_layer = AuthLayer::new(user_store, &secret);
-    //------------------
-
-    // let pool = SqlitePoolOptions::new()
-    //     .connect("db/history.db")
-    //     .await
-    //     .unwrap();
-    //
-    // let user_store = SqliteStore::<User>::new(pool);
-    // let auth_layer = AuthLayer::new(user_store, &secret);
-    //-------------------------------------------------
+    let auth_layer = AuthLayer::new(user_store, &session_secret);
 
     let history = Router::new()
-        // .route("/", get(home))
-        // .route("/lib", get(lib))
-        // .route("/blog", get(blog))
-        .route("/about", get(about))
-        //--------------------
         .route("/protected", get(protected_handler))
-        // .route_layer(RequireAuthorizationLayer::<i64, User>::login())
-        .route_layer(RequireAuth::login())
-        .route("/login", get(login_handler))
-        .route("/logout", get(logout_handler))
+        .route_layer(RequireAuth::login_with_role(Role::Admin..))
+        .route("/login", get(login))
+        .route("/logout", get(logout))
         .route("/", get(home))
+        .route("/lib", get(lib))
+        .route("/blog", get(blog))
         .layer(auth_layer)
         .layer(session_layer)
-        //--------------------
-        .with_state(state)
         .fallback(nothing);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8888));
@@ -175,41 +141,20 @@ async fn blog() -> impl IntoResponse {
     HtmlTemplate(BlogTemplate { posts })
 }
 
-async fn about() -> impl IntoResponse {
-    HtmlTemplate(AboutTemplate {})
-}
-
-//-----------------------------------------------------------
-
 async fn protected_handler(Extension(user): Extension<User>) -> impl IntoResponse {
     format!("Logged in as: {}", user.name)
 }
 
-async fn login_handler(mut auth: Auth) {
-    // let pool = SqlitePoolOptions::new()
-    //     .connect("db/history.db")
-    //     .await
-    //     .unwrap();
-    // let mut conn = pool.acquire().await.unwrap();
-    // let user: User = sqlx::query_as("select * from users where id = 1")
-    //     .fetch_one(&mut conn)
-    //     .await
-    //     .unwrap();
-    let user = User {
-        id: 42,
-        name: "Denis".to_string(),
-        password_hash: "password".to_string(),
-        role: Role::Admin,
-    };
+async fn login(mut auth: Auth) {
+    let secret = "test".to_string();
+    let user = User::new_admin(secret);
     auth.login(&user).await.unwrap();
 }
 
-async fn logout_handler(mut auth: Auth) {
+async fn logout(mut auth: Auth) {
     dbg!("Logging out user: {}", &auth.current_user);
     auth.logout().await;
 }
-
-//-----------------------------------------------------------
 
 async fn nothing() -> impl IntoResponse {
     (StatusCode::NOT_FOUND, "Nothing to see here")
@@ -230,10 +175,6 @@ struct LibTemplate {
 struct BlogTemplate {
     posts: Vec<Post>,
 }
-
-#[derive(Template)]
-#[template(path = "about.html")]
-struct AboutTemplate;
 
 struct HtmlTemplate<T>(T);
 impl<T> IntoResponse for HtmlTemplate<T>
