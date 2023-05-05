7b7bac8f08b16af5e3481139381f9b09f5c93fa3
Denis K <konsistentsi@gmail.com>
2023-05-05 22:36:40 +0700

    WIP: auth

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,14 @@ use axum::{
 use std::{env, net::SocketAddr, sync::Arc};
 
 //-----------------------------------------------------
+use std::collections::HashMap;
+
+use axum_login::{
+    axum_sessions::async_session::MemoryStore as SessionMemoryStore, extractors::AuthContext,
+    memory_store::MemoryStore as AuthMemoryStore,
+};
+use tokio::sync::RwLock;
+//-----------------------------------------------------
 use axum::Extension;
 use axum_login::{
     axum_sessions::{async_session::MemoryStore, SessionLayer},
@@ -17,25 +25,40 @@ use axum_login::{
 use rand::Rng;
 use sqlx::sqlite::SqlitePoolOptions;
 
-#[derive(Debug, Default, Clone, sqlx::FromRow)]
+#[derive(Debug, Clone, sqlx::FromRow)]
 struct User {
-    id: i64,
-    // password_hash: String,
+    id: usize,
     name: String,
+    password_hash: String,
+    role: Role,
 }
 
-impl AuthUser<i64> for User {
-    fn get_id(&self) -> i64 {
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+enum Role {
+    User,
+    Admin,
+}
+
+impl AuthUser<usize, Role> for User {
+    fn get_id(&self) -> usize {
         self.id
     }
 
     fn get_password_hash(&self) -> SecretVec<u8> {
-        // SecretVec::new(self.password_hash.clone().into())
-        SecretVec::new(self.name.clone().into())
+        SecretVec::new(self.password_hash.clone().into())
+    }
+
+    fn get_role(&self) -> Option<Role> {
+        Some(self.role.clone())
     }
 }
 
-type AuthContext = axum_login::extractors::AuthContext<i64, User, SqliteStore<User>>;
+// type AuthContext = axum_login::extractors::AuthContext<i64, User, SqliteStore<User>>;
+// type Auth = AuthContext<usize, User, AuthMemoryStore<usize, User>>;
+// type RequireAuth = RequireAuthorizationLayer<usize, User>;
+
+type Auth = AuthContext<usize, User, AuthMemoryStore<usize, User>, Role>;
+type RequireAuth = RequireAuthorizationLayer<usize, User, Role>;
 
 //-----------------------------------------------------------
 
@@ -62,30 +85,43 @@ async fn main() {
     //-------------------------------------------------
     let secret = rand::thread_rng().gen::<[u8; 64]>();
     let session_store = MemoryStore::new();
-    let session_layer = SessionLayer::new(session_store, &secret).with_secure(false);
+    let session_layer = SessionLayer::new(session_store, &secret);
 
-    let pool = SqlitePoolOptions::new()
-        .connect("db/history.db")
-        .await
-        .unwrap();
+    //------------------
+    let store = Arc::new(RwLock::new(HashMap::default()));
+    let user = User {
+        id: 42,
+        name: "Denis".to_string(),
+        password_hash: "password".to_string(),
+        role: Role::Admin,
+    };
+    store.write().await.insert(user.get_id(), user);
 
-    let user_store = SqliteStore::<User>::new(pool);
+    let user_store = AuthMemoryStore::new(&store);
     let auth_layer = AuthLayer::new(user_store, &secret);
+    //------------------
+
+    // let pool = SqlitePoolOptions::new()
+    //     .connect("db/history.db")
+    //     .await
+    //     .unwrap();
+    //
+    // let user_store = SqliteStore::<User>::new(pool);
+    // let auth_layer = AuthLayer::new(user_store, &secret);
     //-------------------------------------------------
 
     let history = Router::new()
         // .route("/", get(home))
         // .route("/lib", get(lib))
         // .route("/blog", get(blog))
-        // .route("/about", get(about))
-        // .route("/admin", get(admin))
+        .route("/about", get(about))
         //--------------------
         .route("/protected", get(protected_handler))
-        .route_layer(RequireAuthorizationLayer::<i64, User>::login())
+        // .route_layer(RequireAuthorizationLayer::<i64, User>::login())
+        .route_layer(RequireAuth::login())
         .route("/login", get(login_handler))
         .route("/logout", get(logout_handler))
         .route("/", get(home))
-        .route("/about", get(about))
         .layer(auth_layer)
         .layer(session_layer)
         //--------------------
@@ -143,30 +179,32 @@ async fn about() -> impl IntoResponse {
     HtmlTemplate(AboutTemplate {})
 }
 
-async fn admin() -> impl IntoResponse {
-    HtmlTemplate(AdminTemplate {})
-}
-
 //-----------------------------------------------------------
 
 async fn protected_handler(Extension(user): Extension<User>) -> impl IntoResponse {
     format!("Logged in as: {}", user.name)
 }
 
-async fn login_handler(mut auth: AuthContext) {
-    let pool = SqlitePoolOptions::new()
-        .connect("db/history.db")
-        .await
-        .unwrap();
-    let mut conn = pool.acquire().await.unwrap();
-    let user: User = sqlx::query_as("select * from users where id = 1")
-        .fetch_one(&mut conn)
-        .await
-        .unwrap();
+async fn login_handler(mut auth: Auth) {
+    // let pool = SqlitePoolOptions::new()
+    //     .connect("db/history.db")
+    //     .await
+    //     .unwrap();
+    // let mut conn = pool.acquire().await.unwrap();
+    // let user: User = sqlx::query_as("select * from users where id = 1")
+    //     .fetch_one(&mut conn)
+    //     .await
+    //     .unwrap();
+    let user = User {
+        id: 42,
+        name: "Denis".to_string(),
+        password_hash: "password".to_string(),
+        role: Role::Admin,
+    };
     auth.login(&user).await.unwrap();
 }
 
-async fn logout_handler(mut auth: AuthContext) {
+async fn logout_handler(mut auth: Auth) {
     dbg!("Logging out user: {}", &auth.current_user);
     auth.logout().await;
 }
@@ -196,10 +234,6 @@ struct BlogTemplate {
 #[derive(Template)]
 #[template(path = "about.html")]
 struct AboutTemplate;
-
-#[derive(Template)]
-#[template(path = "secret.html")]
-struct AdminTemplate;
 
 struct HtmlTemplate<T>(T);
 impl<T> IntoResponse for HtmlTemplate<T>
